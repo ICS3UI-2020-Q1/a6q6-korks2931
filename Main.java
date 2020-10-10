7ca7cbc027382fa12dfae26ed00fb63d78b40b83
korks2931 <korks2931@wrdsb.ca>
2020-10-10 00:47:57 +0000

    finished a6q6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will show the Fibonacci sequence of a number
+ * @author Sevde
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input = new Scanner(System.in);
+   // ask the user to enter a number to find 
+    System.out.println("What Fibonacci number would you like to find?");
+    // the user will enter the number 
+    int userNumber = input.nextInt();
+    int[] numbers = new int[userNumber];
+    
+    numbers[0] = 1;
+    numbers[1] = 1;
+    // make a for loop 
+    for(int i = 2; i < userNumber; i++){
+      numbers[i] = numbers[i-1] + numbers[i-2];
+    }
+     
+     //tell the user the Fibonacci numbers 
+    System.out.println("The " + userNumber + " Fibonacci number is " + numbers[userNumber - 1]);
+    
+
+    
+
+    }
     
   }
-}
+
